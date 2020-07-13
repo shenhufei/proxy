77166ab3e6b6c101fc9e1f1789a900125cb53ed8
hufei.shen <hufei.shen@ttpai.cn>
2020-07-13 11:42:16 +0800

    1

--- SpringbootApp/src/main/java/springboot/controller/mainController.java
+++ SpringbootApp/src/main/java/springboot/controller/mainController.java
@@ -5,7 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class mainController {
-	
+
+
+
+
+
+
+
+
 	@RequestMapping("/home")
 	public String message() {
 		return "BAM!";
